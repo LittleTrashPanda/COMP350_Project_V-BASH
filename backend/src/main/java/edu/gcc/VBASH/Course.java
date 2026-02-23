ba58cc0e2f2859a0457a786f071ff093bbe329e7
LittleTrashPanda <trueapartment13@gmail.com>
2026-02-23 16:22:25 -0500

    Successful Database Access

--- backend/src/main/java/edu/gcc/VBASH/Course.java
+++ backend/src/main/java/edu/gcc/VBASH/Course.java
@@ -1,28 +1,41 @@
 package edu.gcc.VBASH;
 
 public class Course {
+    // Course Attributes
+    private String courseName;
     private String department;
     private String courseCode;
     private String description;
-    private String professor;
+    private String[] professors;
     private int credits;
-    private int[] days;
+    private int days;
     private int[] startTimes;
     private int[] duration;
     private String semester;
 
-    public Course(String department, String courseCode, String description, String professor, int credits,
-                  int[] days, int[] startTimes, int[] duration, String semester) { }
+    // ----------------------------------------------------------------------------------------------------
 
-    public String getDepartment() { return null; }
-    public String getCourseCode() { return null; }
-    public String getDescription() { return null; }
-    public String getProfessor() { return null; }
-    public int getCredits() { return 0; }
-    public int[] getDays() { return null; }
-    public int[] getStartTimes() { return null; }
-    public int[] getDuration() { return null; }
-    public String getSemester() { return null; }
+    // Constructor
+    public Course(String courseName, String department, String courseCode, String description, String[] professors, int credits, int days, int[] startTimes, int[] duration, String semester) {
+        this.courseName = courseName; this.department = department; this.courseCode = courseCode; this.description = description; this.professors = professors; this.credits = credits; this.days = days; this.startTimes = startTimes; this.duration = duration; this.semester = semester;
+    }
 
+    // ----------------------------------------------------------------------------------------------------
+
+    // Get Attributes
+    public String getCourseName() { return courseName; }
+    public String getDepartment() { return department; }
+    public String getCourseCode() { return courseCode; }
+    public String getDescription() { return description; }
+    public String[] getProfessors() { return professors; }
+    public int getCredits() { return credits; }
+    public int getDays() { return days; }
+    public int[] getStartTimes() { return startTimes; }
+    public int[] getDuration() { return duration; }
+    public String getSemester() { return semester; }
+
+    // ----------------------------------------------------------------------------------------------------
+
+    // Testing Overlap
     public boolean willConflict(Course potentialCourse) { return true; }
 }
--- backend/src/main/java/edu/gcc/VBASH/Search.java
+++ backend/src/main/java/edu/gcc/VBASH/Search.java
@@ -1,28 +1,110 @@
 package edu.gcc.VBASH;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.*;
-import java.util.Map;
+import java.util.*;
 
 public class Search {
     private Map<String, String[]> professorByDepartment;
     private Map<String, String[]> courseCodeByDepartment;
     private String[] keyTerms;
 
-    private Iterable<Course> filteredCourses;
+    private static Iterable<Course> resultingCourses;
 
+    private Iterable<Course> filteredCourses;
     private Filter currentFilter;
-    private Iterable<Course> resultingCourses;
 
     public Iterable<Course> filterCourses (Filter providedFilter) { return null; }
-    public static Iterable<Course> search() throws IOException, ParseException {
+
+    // ----------------------------------------------------------------------------------------------------
+
+    // Accessing Database
+    public static void search() throws IOException, ParseException {
         FileReader sourceFile = new FileReader("backend/src/main/resources/data_wolfe.json");
-        JSONObject classList = (JSONObject) new JSONParser().parse(sourceFile);
-        System.out.print(classList);
-        return null;
+        JSONArray courseList = (JSONArray) ((JSONObject) new JSONParser().parse(sourceFile)).get("classes");
+
+        /*
+        JSONObject test = (JSONObject) courseList.get(new Random().nextInt(courseList.size()));
+
+        System.out.println((String) test.get("name"));
+        System.out.println((String) test.get("subject"));
+        System.out.println(test.get("subject") + test.get("number").toString());
+        System.out.println();
+        System.out.println(Arrays.toString(professorParsing((JSONArray) test.get("faculty"))));
+        System.out.println(Math.toIntExact((long) test.get("credits")));
+        System.out.println(dayParsing((JSONArray) test.get("times")));
+        System.out.println(Arrays.toString(startTimeParsing((JSONArray) test.get("times"))));
+        System.out.println(Arrays.toString(durationParsing((JSONArray) test.get("times"))));
+        System.out.println((String) test.get("semester"));
+        */
+
+        ArrayList<Course> toReturn = new ArrayList<Course>();
+        for (Object course : courseList) { toReturn.add(courseCreator((JSONObject) course)); }
+        resultingCourses = toReturn;
+    }
+
+    // Helper Methods
+    private static Course courseCreator(JSONObject course) {
+        return new Course(
+                course.get("name").toString(),
+                course.get("subject").toString(),
+                (course.get("subject") + course.get("number").toString()),
+                "No Description Provided",
+                professorParsing((JSONArray) course.get("faculty")),
+                Math.toIntExact((long) course.get("credits")),
+                dayParsing((JSONArray) course.get("times")),
+                startTimeParsing((JSONArray) course.get("times")),
+                durationParsing((JSONArray) course.get("times")),
+                course.get("semester").toString()
+        );
+    }
+
+    // Transforming Data
+    private static String[] professorParsing(JSONArray professors) {
+        String[] toReturn = new String[professors.size()];
+        for (int i = 0; i < professors.size(); i++) { toReturn[i] = professors.get(i).toString(); }
+        return toReturn;
+    }
+
+    private static int dayParsing(JSONArray dates) {
+        int toReturn = 1;
+        Map<String, Integer> dayMap = Map.of("M", 2, "T", 3, "W", 5, "R", 7, "F", 11);
+
+        for (Object date : dates) { toReturn *= dayMap.get(((JSONObject) date).get("day").toString()); }
+
+        return toReturn;
+    }
+
+    private static int[] startTimeParsing(JSONArray dates) {
+        int[] toReturn = new int[5];
+        Map<String, Integer> dayMap = Map.of("M", 0, "T", 1, "W", 2, "R", 3, "F", 4);
+
+        for (Object date : dates) { toReturn[dayMap.get(((JSONObject) date).get("day").toString())] = timeParsing(((JSONObject) date).get("end_time").toString()); }
+        return toReturn;
+    }
+
+    private static int[] durationParsing(JSONArray dates) {
+        int[] toReturn = new int[5];
+        Map<String, Integer> dayMap = Map.of("M", 0, "T", 1, "W", 2, "R", 3, "F", 4);
+
+        for (Object date : dates) { toReturn[dayMap.get(((JSONObject) date).get("day").toString())] = timeParsing((String) ((JSONObject) date).get("end_time")) - timeParsing((String) ((JSONObject) date).get("start_time")); }
+        return toReturn;
+    }
+
+    private static int timeParsing(String time) {
+        String text = new Scanner(time).nextLine();
+        return (((int) text.charAt(0)) * 10 + ((int) text.charAt(1))) * 60 + ((int) text.charAt(3) * 10 + (int) text.charAt(4));
+    }
+
+    // ----------------------------------------------------------------------------------------------------
+
+    public static void main(String[] args) throws IOException, ParseException {
+        try { search(); }
+        catch (Exception e) { System.out.print(e); }
     }
 
     public void resetFilter(){
