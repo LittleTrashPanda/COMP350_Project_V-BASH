33f197fcd37ef8d26736da4ecd13fcffc4f6befa
LittleTrashPanda <trueapartment13@gmail.com>
2026-02-26 14:47:30 -0500

    Added /calendar Path to Javalin
    Created Calendar HTML, CSS, and JS Files
    
    Added Handling for Receiving Schedule Data
    
    Added Element Creation when Receiving Schedule Data
    
    Added Element Positioning when Receiving Schedule Data

--- backend/src/main/java/edu/gcc/VBASH/Schedule.java
+++ backend/src/main/java/edu/gcc/VBASH/Schedule.java
@@ -1,18 +1,30 @@
 package edu.gcc.VBASH;
 
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Schedule {
     private String name;
-    private Iterable<Course> courses;
+    private static Iterable<Course> courses;
 
     public Schedule(String name, Iterable<Course> courses) { }
 
-    public boolean checkCourseConflict(Course potentialCourse) { return true; }
+    public boolean checkCourseConflict(Course potentialCourse) { return false; }
     public void addCourse(Course potentialCourse) { return; }
     public void removeCourse(Course enrolledCourse) { return; }
     public void replaceCourse(Course oldCourse, Course newCourse) { return; }
 
-    public Iterable<Course> getCourses() { return null; }
+    public static Iterable<Course> getCourses() throws IOException, ParseException {
+        ArrayList<Course> test = new ArrayList<Course>();
+
+        test.add((Course) ((ArrayList) Search.search()).get(180));
+        test.add((Course) ((ArrayList) Search.search()).get(400));
+        return test;
+    }
 
     public String getName() { return null; }
-    public void setName(String newName) { return; }
+    public void setName(String newName) { name = newName; }
 }
---
+++ backend/src/main/resources/public/calendar.js
@@ -0,0 +1,91 @@
+/* Document Creation */
+document.addEventListener("DOMContentLoaded", async () => {
+    loadSchedule();
+});
+
+/* Display Courses */
+async function loadSchedule() {
+    const res = await fetch("/calendar");
+    const scheduledCourses = await res.json();
+
+    const calendarDays = document.getElementById("calendar").children;
+
+    for (const course of scheduledCourses) {
+        console.log(course);
+
+        // Monday
+        if (course.days % 2 == 0) {
+            const block = document.createElement("div");
+            const courseTitle = document.createElement("p");
+            courseTitle.textContent = course.courseName;
+            courseTitle.classList.add("dayCourseTitle");
+            block.appendChild(courseTitle);
+
+            block.style.top = ((course.startTimes[0] - 8 * 60) / (14 * 60)) * 100 + "%";
+            block.style.height = (course.duration[0] / (14 * 60)) * 100 + "%";
+            block.classList.add("dayCourse");
+
+            calendarDays[0].children[1].appendChild(block);
+        }
+
+        // Tuesday
+        if (course.days % 3 == 0) {
+            const block = document.createElement("div");
+            const courseTitle = document.createElement("p");
+            courseTitle.textContent = course.courseName;
+            courseTitle.classList.add("dayCourseTitle");
+            block.appendChild(courseTitle);
+
+            block.style.top = ((course.startTimes[1] - 8 * 60) / (14 * 60)) * 100 + "%";
+            block.style.height = (course.duration[1] / (14 * 60)) * 100 + "%";
+            block.classList.add("dayCourse");
+
+            calendarDays[1].children[1].appendChild(block);
+        }
+
+        // Wednesday
+        if (course.days % 5 == 0) {
+            const block = document.createElement("div");
+            const courseTitle = document.createElement("p");
+            courseTitle.textContent = course.courseName;
+            courseTitle.classList.add("dayCourseTitle");
+            block.appendChild(courseTitle);
+
+            block.style.top = ((course.startTimes[2] - 8 * 60) / (14 * 60)) * 100 + "%";
+            block.style.height = (course.duration[2] / (14 * 60)) * 100 + "%";
+            block.classList.add("dayCourse");
+
+            calendarDays[2].children[1].appendChild(block);
+        }
+
+        // Thursday
+        if (course.days % 7 == 0) {
+            const block = document.createElement("div");
+            const courseTitle = document.createElement("p");
+            courseTitle.textContent = course.courseName;
+            courseTitle.classList.add("dayCourseTitle");
+            block.appendChild(courseTitle);
+
+            block.style.top = ((course.startTimes[3] - 8 * 60) / (14 * 60)) * 100 + "%";
+            block.style.height = (course.duration[3] / (14 * 60)) * 100 + "%";
+            block.classList.add("dayCourse");
+
+            calendarDays[3].children[1].appendChild(block);
+        }
+
+        // Friday
+        if (course.days % 11 == 0) {
+            const block = document.createElement("div");
+            const courseTitle = document.createElement("p");
+            courseTitle.textContent = course.courseName;
+            courseTitle.classList.add("dayCourseTitle");
+            block.appendChild(courseTitle);
+
+            block.style.top = ((course.startTimes[4] - 8 * 60) / (14 * 60)) * 100 + "%";
+            block.style.height = (course.duration[4] / (14 * 60)) * 100 + "%";
+            block.classList.add("dayCourse");
+
+            calendarDays[4].children[1].appendChild(block);
+        }
+    }
+}
