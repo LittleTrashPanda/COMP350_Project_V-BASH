38c93b8395f6c280a6816b0bcbf2d8e576a81db6
LittleTrashPanda <trueapartment13@gmail.com>
2026-02-27 09:21:10 -0500

    Fixed Null Instance Issue with Schedule Courses

--- backend/src/main/java/edu/gcc/VBASH/Schedule.java
+++ backend/src/main/java/edu/gcc/VBASH/Schedule.java
@@ -3,10 +3,11 @@ package edu.gcc.VBASH;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Schedule {
     private String name;
-    private static Iterable<Course> courses;
+    private static Iterable<Course> courses = new ArrayList<Course>();
 
     public Schedule(String name, Iterable<Course> courses) { }
 
