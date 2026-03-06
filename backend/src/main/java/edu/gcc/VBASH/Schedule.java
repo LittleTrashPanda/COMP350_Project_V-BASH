3f77072ef0fbe7cedd22a9b72daf5710dda1aa57
Brehm Moritz <moritzbs22@gcc.edu>
2026-03-06 17:50:48 -0500

    added addCourse

--- backend/src/main/java/edu/gcc/VBASH/Schedule.java
+++ backend/src/main/java/edu/gcc/VBASH/Schedule.java
@@ -12,7 +12,26 @@ public class Schedule {
     public Schedule(String name, Iterable<Course> courses) { }
 
     public boolean checkCourseConflict(Course potentialCourse) { return false; }
-    public void addCourse(Course potentialCourse) { return; }
+    public void addCourse(Course potentialCourse) {
+        //checks if there is a course conflict. If there is, the method does nothing yet.
+        if (!checkCourseConflict(potentialCourse)){
+
+            //Maybe not needed, but creates a copy of courses and a mutable schedule list since we can't
+            //add directly to courses
+            ArrayList<Course> tempSchedule = new ArrayList<Course>();
+            Iterable<Course> courseCopy = new ArrayList<Course>();
+            courseCopy = courses;
+
+            //goes through the copied list of courses we now have, and fills out the temporary schedule
+            while(courseCopy.iterator().hasNext()) {
+                tempSchedule.add(courseCopy.iterator().next());
+            }
+            //Adds the potential course, and sets the values of courses equal to the tempSchedule
+            tempSchedule.add(potentialCourse);
+            courses = tempSchedule;
+
+        }
+         }
     public void removeCourse(Course enrolledCourse) { return; }
     public void replaceCourse(Course oldCourse, Course newCourse) { return; }
 
