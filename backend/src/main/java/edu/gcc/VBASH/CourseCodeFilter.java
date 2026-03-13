4f2b627e432867a30257011db05afce262bbd1be
NikiRhine <160781413+NikiRhine@users.noreply.github.com>
2026-03-12 22:38:54 -0400

    CourseCodeFilter, DeptFilter, CreditNumFilter working, need tested

--- backend/src/main/java/edu/gcc/VBASH/CourseCodeFilter.java
+++
@@ -1,6 +0,0 @@
-package edu.gcc.VBASH;
-
-public class CourseCodeFilter {
-
-    // Course Code Filter - adding for class
-}
--- backend/src/main/java/edu/gcc/VBASH/CreditNumFilter.java
+++
@@ -1,4 +0,0 @@
-package edu.gcc.VBASH;
-
-public class CreditNumFilter {
-}
