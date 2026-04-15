173165ac4ae63b7e333b062d4c653c6664a1062c
sheldondeal812 <sheldondeal812@gmail.com>
2026-04-15 19:09:10 -0400

    React

---
+++ classReact/frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import SearchPage from "./pages/SearchPage.jsx";
+// import CalendarPage from "./pages/CalendarPage.jsx";
+import "./App.css";
+
+export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={() => setIsOpen(!isOpen)} />
+      <div
+        className="main-content"
+        style={{
+          marginLeft: isOpen ? "240px" : "100px",
+          transition: "margin-left 0.3s ease",
+          width: "100%",
+        }}
+      >
+        <Routes>
+          <Route path="/" element={
+              <SearchPage />
+              }/>
+        </Routes>
+      </div>
+    </>
+  );
+}
